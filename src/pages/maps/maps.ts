4eee2163c04ffaf8cc8caaa7988aa548ed12d351
jhonny9550 <jhonny9550@hotmail.com>
2016-11-02 08:45:25 -0500

    Trazado de rutas implementado

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,6 +1,7 @@
 import { Component,ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Geolocation} from 'ionic-native';
+import {map} from "rxjs/operator/map";
 
 declare var google;
 
@@ -12,27 +13,48 @@ export class Maps {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  origen:any;
+  destination:any;
+  public directionsDisplay;
 
-  constructor(public navCtrl: NavController) {}
+  directionsService:any = new google.maps.DirectionsService();
+
+  constructor(public navCtrl: NavController) {
+  }
 
   ionViewDidLoad() {
+   this.directionsDisplay = new google.maps.DirectionsRenderer();
     this.loadMap();
+  }
+
+  addRoute(directionsDisplay){
+      var request = {
+        origin:this.origen,
+        destination:this.destination,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      this.directionsService.route(request,function(result,status){
+        if (status == google.maps.DirectionsStatus.OK) {
+          console.log("intentando trasar ruta");
+          directionsDisplay.setDirections(result);
+        }
+
+      });
   }
 
   loadMap(){
 
     Geolocation.getCurrentPosition().then((position) => {
-
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
+      this.origen = latLng;
+        let mapOptions = {
         center: latLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
+      this.directionsDisplay.setMap(this.map);
       console.log("Geo funcionando con: "+position);
 
     }, (err) => {
